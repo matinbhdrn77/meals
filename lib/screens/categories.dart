609c78eab6b00f506b67141bb4a912a1c180707d
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-29 19:23:25 +0330

    create a screen to show all of our categories

---
+++ lib/screens/categories.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Choose Your Category')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          Text('1'),
+          Text('2'),
+          Text('13'),
+          Text('4'),
+        ],
+      ),
+    );
+  }
+}
