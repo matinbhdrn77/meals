d512a77b5b7bff361c2282f95f21f17b3b2bd658
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-02 09:55:20 +0330

    a widget for every item in selected caategory

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -16,9 +17,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemBuilder: ((context, index) {
-        return Container(child: Column(children: [
-          Text(meals[index].title),
-        ]),);
+        return MealItem(meal: meals[index]);
       }),
       itemCount: meals.length,
     );
