bef62e130c84c92ad6797d9e7a0d145ea9ce5325
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-02 12:02:27 +0330

    remove scaffold because it cause 2 app bar

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -22,9 +22,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Choose Your Category')),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -40,7 +38,6 @@ class CategoriesScreen extends StatelessWidget {
                   },
                 ))
             .toList(),
-      ),
-    );
+      );
   }
 }
