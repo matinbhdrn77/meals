f1336ddd8b8d6c0a21bf62e0405902d6572f56c8
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-02 12:02:59 +0330

    change it so we can use it as a fav screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -7,11 +7,11 @@ import 'package:meals/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void _selectMeal(BuildContext context, Meal meal) {
@@ -59,8 +59,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    // if MealsScreen used for favourite screen
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(title!)),
       body: content,
     );
   }
