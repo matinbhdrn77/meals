18985e84cc324ffe00055e6e73b4ae8a02d1ba52
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-02 16:00:10 +0330

    widget for our menu bar in main screen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -3,6 +3,7 @@ import 'package:meals/models/meal.dart';
 
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -64,6 +65,7 @@ class _TabsScreenState extends State<TabsScreen> {
         title: Text(activePageTitle),
       ),
       body: activePage,
+      drawer: const MainDrawer(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
