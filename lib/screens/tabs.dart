ee97cb8131bbd20659ab1a6fe9944d84e21c040e
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-02 13:09:49 +0330

    function for adding and removing meals to favotites

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
 
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
@@ -12,6 +13,17 @@ class TabsScreen extends StatefulWidget {
 
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
+  final List<Meal> _favouriteMeals = [];
+
+  void _toggleMealFavouriteStatus(Meal meal) {
+    final isExist = _favouriteMeals.contains(meal);
+
+    if (isExist) {
+      _favouriteMeals.remove(meal);
+    } else {
+      _favouriteMeals.add(meal);
+    }
+  }  
 
   void _selectPage(int index) {
     setState(() {
@@ -21,11 +33,11 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Widget activePage = const CategoriesScreen();
+    Widget activePage = CategoriesScreen(onToggleFavourite: _toggleMealFavouriteStatus,);
     String activePageTitle = 'Categories';
 
     if (_selectedPageIndex == 1) {
-      activePage = const MealsScreen(meals: []);
+      activePage = MealsScreen(meals: _favouriteMeals, onToggleFavourite: _toggleMealFavouriteStatus,);
       activePageTitle = 'Your Favourite';
     }
 
